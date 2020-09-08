356e59a4b7e55db86d0aa72f92cec9c14527072e
jonghyeonkim <whdgus8219@naver.com>
2020-09-08 22:55:58 +0900

    first commit

---
+++ src/main/kotlin/com/jonghyeon/lightning/infra/config/SecurityConfig.kt
@@ -0,0 +1,18 @@
+package com.jonghyeon.lightning.infra.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests()
+                .mvcMatchers("/", "/login", "/sign-up", "/check-emil-token",
+                "/email-login", "/login-by-email").permitAll()
+                .anyRequest().authenticated()
+    }
+}
---
+++ src/main/kotlin/com/jonghyeon/lightning/modules/account/UserAccount.kt
@@ -0,0 +1,4 @@
+package com.jonghyeon.lightning.modules.account
+
+class UserAccount {
+}
